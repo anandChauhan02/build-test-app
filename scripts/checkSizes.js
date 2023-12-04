2e175b5919381f0d723b71a3fbbf33c5e52645dd
anandChauhan02 <anand.chauhan@logixal.com>
2023-12-04 19:13:05 +0530

    add code changes

--- scripts/checkSizes.js
+++ scripts/checkSizes.js
@@ -1,48 +1,50 @@
-const fs = require('fs');
+// scripts/checkSizes.js
+const fs = require('fs').promises; // Use promises version of fs
 const path = require('path');
 
-const checkFileSize = (filePath, maxSize) => {
-    console.log("check--01")
+const checkFileSize = async (filePath, maxSize) => {
+    console.log("check--01");
     try {
-        const fileSize = fs.statSync(filePath).size;
+        const stats = await fs.stat(filePath);
+        const fileSize = stats.size;
+
         if (fileSize > maxSize) {
-            console.error(`File size exceeded limit: ${filePath}`);
-            process.exit(1);
+            return `File size exceeded limit: ${filePath}`;
         }
-        console.log(`File size is within limit: ${filePath}`);
+        return `File size is within limit: ${filePath}`;
     } catch (error) {
-        console.error(`Error checking file size for ${filePath}:`, error);
-        process.exit(1);
+        return `Error checking file size for ${filePath}: ${error.message}`;
     }
 };
 
-const checkFileSizes = () => {
-    const buildPath = path.resolve(__dirname, '../build/static');
-    console.log("check--02")
-    // Get the filenames dynamically
+const checkFileSizes = async (buildPath) => {
+    console.log("check--02");
+
     const jsPath = path.join(buildPath, 'js');
     const cssPath = path.join(buildPath, 'css');
 
+    const messages = [];
+
     try {
-        const jsFilenames = fs.readdirSync(jsPath).filter((file) => file.endsWith('.js'));
-        const cssFilenames = fs.readdirSync(cssPath).filter((file) => file.endsWith('.css'));
+        const jsFilenames = await fs.readdir(jsPath);
+        const cssFilenames = await fs.readdir(cssPath);
 
         if (jsFilenames.length === 0 || cssFilenames.length === 0) {
-            console.error('Error: Could not find JS or CSS files in the build directory.');
-            process.exit(1);
+            messages.push('Error: Could not find JS or CSS files in the build directory.');
+        } else {
+            for (const jsFilename of jsFilenames) {
+                messages.push(await checkFileSize(path.join(jsPath, jsFilename), 300 * 1024));
+            }
+
+            for (const cssFilename of cssFilenames) {
+                messages.push(await checkFileSize(path.join(cssPath, cssFilename), 300 * 1024));
+            }
         }
-
-        jsFilenames.forEach((jsFilename) => {
-            checkFileSize(path.join(jsPath, jsFilename), 300 * 1024); // Adjust the size limit accordingly
-        });
-
-        cssFilenames.forEach((cssFilename) => {
-            checkFileSize(path.join(cssPath, cssFilename), 300 * 1024); // Adjust the size limit accordingly
-        });
     } catch (error) {
-        console.error('Error checking file sizes:', error);
-        process.exit(1);
+        messages.push(`Error checking file sizes: ${error.message}`);
     }
+
+    return messages;
 };
 
-checkFileSizes();
+module.exports = checkFileSizes;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const SizePlugin = require('size-plugin');
+const path = require('path');
+const checkFileSizes = require('./scripts/checkSizes');
 
 module.exports = {
-    // other webpack configurations...
+    // ... other webpack configurations ...
 
     plugins: [
-        // other plugins...
+        // ... other plugins ...
         new BundleAnalyzerPlugin({
             analyzerMode: 'static',
             reportFilename: 'bundle-analyzer-report.html',
@@ -12,77 +17,38 @@ module.exports = {
             generateStatsFile: true,
             statsFilename: 'bundle-analyzer-stats.json',
         }),
-    ],
-
-    performance: {
-        hints: process.env.NODE_ENV === 'production' ? 'warning' : false,
-        maxAssetSize: 300 * 1024, // 300 KB
-        maxEntrypointSize: 300 * 1024, // 300 KB
-    },
-};
-
-
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-
-module.exports = {
-    // other webpack configurations...
-
-    plugins: [
-        // other plugins...
         new MiniCssExtractPlugin(),
+        new SizePlugin({ writeFile: false }), // Disable writing sizes to a file (we will handle this manually)
     ],
 
     optimization: {
         minimizer: [
             new CssMinimizerPlugin(),
-            new SizePlugin({ writeFile: false }), // Disable writing sizes to a file (we will handle this manually)
         ],
     },
 
     module: {
         rules: [
-            // other rules...
+            // ... other rules ...
             {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
         ],
     },
-};
-
-const path = require('path');
-
-module.exports = {
-    // other configurations...
 
     output: {
         path: path.resolve(__dirname, 'build/static'),
         filename: 'js/main.js',
-        chunkFilename: 'js/[name].[contenthash].chunk.js', // Use hashes for dynamic imports
+        chunkFilename: 'js/[name].[contenthash].chunk.js',
         publicPath: '/',
     },
-};
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-module.exports = {
-    // other configurations...
-
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/main.css',
-            chunkFilename: 'css/[name].[contenthash].css', // Use hashes for dynamically imported styles
-        }),
-    ],
-
-    module: {
-        rules: [
-            // other rules...
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-        ],
+    // This hook runs after the webpack build process is completed
+    // You can use it to check file sizes
+    hooks: {
+        afterEmit: () => {
+            checkFileSizes(path.resolve(__dirname, 'build/static'));
+        },
     },
 };
